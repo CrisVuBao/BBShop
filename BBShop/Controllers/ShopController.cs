dc44158965c141ffe6a7b2a4270e98f0f99d2ad8
Cris <mobile02022002@gmail.com>
2024-02-19 15:03:15 +0700

    Đã hoàn thành xử lý view component cho danh sách loại sản phẩm

---
+++ BBShop/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BBShop.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index(int? loai)
+        {
+            return View();
+        }
+    }
+}
---
+++ BBShop/ViewComponents/MenuLoaiViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using BBShop.Data;
+using BBShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BBShop.ViewComponents
+{
+    public class MenuLoaiViewComponent : ViewComponent
+    {
+        private readonly BbshopContext _db;
+
+        public MenuLoaiViewComponent(BbshopContext context)
+        {
+            _db = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var data = _db.Loais.Select(x => new MenuLoaiVM
+            {
+                MaLoai = x.MaLoai,
+                TenLoai = x.TenLoai,
+                SoLuong = x.HangHoas.Count
+            });
+
+            return View(data); // mặc định của action view là default.cshtml, nếu muốn đổi tên khác thì View("_tên muốn đổi_",data)
+        }
+    }
+}
